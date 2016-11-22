0c81cc0abbc28aad1fd8b86eef74c552fb820396
kiririmode <kiririmode@gmail.com>
2016-11-23 06:32:45 +0900

    add go environment initialization.

---
+++ etc/init/scripts/go.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Stop script if errors occur
+trap 'echo Error: $0:$LINENO stopped; exit 1' ERR INT
+set -eu
+
+# Load vital library that is most important and constructed with many
+# minimal functions.
+. "$DOTPATH"/etc/lib/vital.sh
+
+imports=(
+    # An autocompletion daemon for the Go programming language
+    github.com/nsf/gocode
+    # Print where symbols are defined in Go source code
+    github.com/rogpeppe/godef
+)
+
+for i in ${imports[@]}; do
+    if go get -u "$i"; then
+        log_pass "$i is installed successfully"
+    else
+        log_fail "error: failed to install $i"
+    fi
+done
