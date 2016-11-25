d07648211ba5967c05361cb796778aa89a7e62e9
kiririmode <kiririmode@gmail.com>
2016-11-26 02:45:18 +0900

    make helm-ag-project-root finds hidden files.

--- .emacs.d/inits/015-helm.el
+++ .emacs.d/inits/015-helm.el
@@ -104,7 +104,11 @@
  ; Insert thing at point as search pattern.
  helm-ag-insert-at-point 'symbol
  ; Enable fuzzy match
- helm-ag-fuzzy-match t)
+ helm-ag-fuzzy-match t
+ ; Command line option of `ag'. This is appended after `helm-ag-base-command'
+ helm-ag-command-option "--hidden")
+
+(global-set-key (kbd "M-p") 'helm-ag-project-root)
 
 ;; helm-descbinds
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
