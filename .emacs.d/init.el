cc4b4ae2fd43f0c3ef1510f52bc2533a7cc4431d
kiririmode <kiririmode@gmail.com>
2017-03-20 04:58:19 +0900

    rename path.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,6 +2,13 @@
 ;;;
 ;;; Cask is a project management tool for Emacs that helps automate the
 ;;; package development cycle.
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (require 'cask "~/.cask/cask.el")
 (require 'eieio)
 
