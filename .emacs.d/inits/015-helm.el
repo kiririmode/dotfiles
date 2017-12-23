e40bc11d3d25b4796649bce85ea4b4a305648bf3
kiririmode <kiririmode@gmail.com>
2017-12-23 13:12:23 +0900

    introduce helm-git-grep to emacs.

--- .emacs.d/inits/015-helm.el
+++ .emacs.d/inits/015-helm.el
@@ -118,3 +118,13 @@
 (helm-descbinds-mode)
 
 (global-set-key (kbd "C-c b") 'helm-descbinds)
+
+;; helm-git-grep
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'helm-git-grep)
+(global-set-key (kbd "C-c g") 'helm-git-grep)
+;; Invoke `helm-git-grep' from isearch.
+(define-key isearch-mode-map (kbd "C-c g") 'helm-git-grep-from-isearch)
+;; Invoke `helm-git-grep' from other helm.
+(eval-after-load 'helm
+  '(define-key helm-map (kbd "C-c g") 'helm-git-grep-from-helm))
