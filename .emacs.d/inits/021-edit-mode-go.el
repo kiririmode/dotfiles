a19d30128bd8045d6995ea01294becb9b667357f
kiririmode <kiririmode@gmail.com>
2018-04-11 15:06:24 +0900

    with-eval-after-load doesn't need progn.

--- .emacs.d/inits/021-edit-mode-go.el
+++ .emacs.d/inits/021-edit-mode-go.el
@@ -1,43 +1,42 @@
 (with-eval-after-load "go-mode"
-  '(progn
-     (add-to-list 'exec-path (expand-file-name "~/bin"))
+  (add-to-list 'exec-path (expand-file-name "~/bin"))
 
-     ; Add this to .emacs to run gofmt on the current buffer when saving:
-     (add-hook 'before-save-hook 'gofmt-before-save)
+  ; Add this to .emacs to run gofmt on the current buffer when saving:
+  (add-hook 'before-save-hook 'gofmt-before-save)
 
-     ; golint is a linter for Go source code.
-     (add-to-list 'load-path (concat (getenv "GOPATH") "/src/github.com/golang/lint/misc/emacs"))
-     (require 'golint)
-     (add-to-list 'load-path (concat (getenv "GOPATH") "/src/github.com/dougm/goflymake"))
-     (require 'go-flycheck)
+   ; golint is a linter for Go source code.
+  (add-to-list 'load-path (concat (getenv "GOPATH") "/src/github.com/golang/lint/misc/emacs"))
+  (require 'golint)
+  (add-to-list 'load-path (concat (getenv "GOPATH") "/src/github.com/dougm/goflymake"))
+  (require 'go-flycheck)
 
-     (require 'go-autocomplete)
-     (ac-config-default)
-     (require 'go-eldoc)
-     (add-hook 'go-mode-hook 'go-eldoc-setup)
-     (add-hook 'go-mode-hook 'flycheck-mode)
-     (add-hook 'go-mode-hook (lambda()
-                               ; The 'gofmt' command.
-                               (setq gofmt-command "goimports")))
+  (require 'go-autocomplete)
+  (ac-config-default)
+  (require 'go-eldoc)
+  (add-hook 'go-mode-hook 'go-eldoc-setup)
+  (add-hook 'go-mode-hook 'flycheck-mode)
+  (add-hook 'go-mode-hook (lambda()
+                            ; The 'gofmt' command.
+                            (setq gofmt-command "goimports")))
 
-     ; http://syohex.hatenablog.com/entry/20130618/1371567527
-     (defvar my/helm-go-source
-       '((name . "Helm Go")
-         (candidates . go-packages)
-         (action . (("Show document" . godoc)
-                    ("Import package" . my/helm-go-import-add)))))
+  ; http://syohex.hatenablog.com/entry/20130618/1371567527
+  (defvar my/helm-go-source
+    '((name . "Helm Go")
+      (candidates . go-packages)
+      (action . (("Show document" . godoc)
+                 ("Import package" . my/helm-go-import-add)))))
 
-     (defun my/helm-go-import-add (candidate)
-       (dolist (package (helm-marked-candidates))
-         (go-import-add current-prefix-arg package)))
+  (defun my/helm-go-import-add (candidate)
+    (dolist (package (helm-marked-candidates))
+      (go-import-add current-prefix-arg package)))
 
-     (defun my/helm-go ()
-       (interactive)
-       (helm :sources '(my/helm-go-source) :buffer "*helm go*"))
+  (defun my/helm-go ()
+    (interactive)
+    (helm :sources '(my/helm-go-source) :buffer "*helm go*"))
 
-     ; Jump to the definition of the expression at POINT.
-     (define-key go-mode-map (kbd "M-.") 'godef-jump-other-window)
-     ; Show Go documentation for the identifier at POINT.
-     (define-key go-mode-map (kbd "C-.") 'godoc-at-point)
+  ; Jump to the definition of the expression at POINT.
+  (define-key go-mode-map (kbd "M-.") 'godef-jump-other-window)
+  ; Show Go documentation for the identifier at POINT.
+  (define-key go-mode-map (kbd "C-.") 'godoc-at-point)
 
-     (define-key go-mode-map (kbd "C-c C-d") 'my/helm-go)))
+  (define-key go-mode-map (kbd "C-c C-d") 'my/helm-go))
