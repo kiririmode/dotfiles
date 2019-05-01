2831c94168e8c4818c716bbc1b189fd6cbb9ce88
kiririmode <kiririmode@gmail.com>
2019-05-01 23:59:21 +0900

    スクリーンショット保存ディレクトリの変更

--- etc/init/osx/04_defaults.sh
+++ etc/init/osx/04_defaults.sh
@@ -2,5 +2,9 @@
 
 # show hidden files in Finder app.
 defaults write com.apple.finder AppleShowAllFiles TRUE
-
 killall Finder
+
+# スクリーンショットを配置する場所を変更する
+mkdir -p ~/Pictures/screenshot
+defaults write com.apple.screencapture location ~/Pictures/screenshot
+killall SystemUIServer
