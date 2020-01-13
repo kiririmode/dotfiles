0fea0b02251553fb657f3075590807922f7f1c9c
kiririmode <kiririmode@gmail.com>
2020-01-13 15:04:50 +0900

    fix: update blogsync to v0.10.3

--- etc/init/scripts/install-go-tools.sh
+++ etc/init/scripts/install-go-tools.sh
@@ -7,7 +7,7 @@ source $DOTPATH/etc/lib/vital.sh
 readonly BIN_DIR="$HOME/bin"
 
 function install_blogsync() {
-    readonly BLOGSYNC_VER="v0.10.2"
+    readonly BLOGSYNC_VER="v0.10.3"
     log_info "installing blogsync ${BLOGSYNC_VER}..."
     curl --progress-bar --location --output "${TMPDIR}blogsync.zip" --remote-name https://github.com/motemen/blogsync/releases/download/${BLOGSYNC_VER}/blogsync_${BLOGSYNC_VER}_darwin_amd64.zip
     unzip -oj "${TMPDIR}blogsync.zip" blogsync_v0.10.2_darwin_amd64/blogsync -d $HOME/bin
