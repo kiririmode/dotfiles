342ebac0d9daedcabe649473ec5f5852907a66fe
kiririmode <kiririmode@gmail.com>
2020-02-29 13:36:48 +0900

    add: GOPATH environment variable

--- .zsh.d/go.zsh
+++ .zsh.d/go.zsh
@@ -3,9 +3,17 @@
 if has goenv; then
     path=(
         $HOME/.goenv/bin(N-/)
+        $GOPATH/bin
         $path
     )
 fi
 eval "$(goenv init -)"
+
+typeset -xT GOPATH gopath
+typeset -U gopath
+gopath=(
+    $gopath
+    $HOME
+)
 
 e_done "setting go environment"
