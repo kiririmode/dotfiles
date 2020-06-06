6ebb9b44c48b7b668a9536c658fb705efb5f1d84
kiririmode <kiririmode@gmail.com>
2020-06-07 05:01:36 +0900

    fix: hard coded blogsync version

--- etc/init/scripts/install-go-tools.sh
+++ etc/init/scripts/install-go-tools.sh
@@ -10,7 +10,7 @@ function install_blogsync() {
     readonly BLOGSYNC_VER="v0.10.3"
     log_info "installing blogsync ${BLOGSYNC_VER}..."
     curl --progress-bar --location --output "${TMPDIR}blogsync.zip" --remote-name https://github.com/motemen/blogsync/releases/download/${BLOGSYNC_VER}/blogsync_${BLOGSYNC_VER}_darwin_amd64.zip
-    unzip -oj "${TMPDIR}blogsync.zip" blogsync_v0.10.2_darwin_amd64/blogsync -d $HOME/bin
+    unzip -oj "${TMPDIR}blogsync.zip" blogsync_${BLOGSYNC_VER}_darwin_amd64/blogsync -d $HOME/bin
     e_done "blogsync ${BLOGSYNC_VER}"
 }
 
