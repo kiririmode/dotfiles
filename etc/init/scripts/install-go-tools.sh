9a219fb00067e6b7ef439320c2706bc8711f4a42
kiririmode <kiririmode@gmail.com>
2022-08-11 18:15:06 +0900

    update: ver.up

--- etc/init/scripts/install-go-tools.sh
+++ etc/init/scripts/install-go-tools.sh
@@ -7,7 +7,9 @@ source $DOTPATH/etc/lib/vital.sh
 readonly BIN_DIR="$HOME/bin"
 
 function install_blogsync() {
-    readonly BLOGSYNC_VER="v0.10.3"
+    readonly BLOGSYNC_VER="v0.12.0"
+
+    mkdir -p "${HOME}/bin"
     log_info "installing blogsync ${BLOGSYNC_VER}..."
     curl --progress-bar --location --output "${TMPDIR}blogsync.zip" --remote-name https://github.com/motemen/blogsync/releases/download/${BLOGSYNC_VER}/blogsync_${BLOGSYNC_VER}_darwin_amd64.zip
     unzip -oj "${TMPDIR}blogsync.zip" blogsync_${BLOGSYNC_VER}_darwin_amd64/blogsync -d $HOME/bin
